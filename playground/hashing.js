020f2ef1a3461381fae229b3e06a028110213cc1
Philippe Loup <philippe.loup@gmail.com>
2018-04-25 09:01:28 -0400

    Hash user Password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,31 @@
+
+
+
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
+//
+// bcrypt.genSalt(10, (err,salt) => {
+//   bcrypt.hash(password,salt,(err, hash) =>{
+//     console.log(hash);
+//   })
+// });
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
-var decode = jwt.verify(token,'123abc');
-console.log('Decoded: ', decode);
+var hashedPassword = '$2a$10$q4C6uIHhEi2WfuydzHOSYONjI7lmoapIvQGdSoa48nAi/5KUx2Fwa'
+
+bcrypt.compare(password, hashedPassword,(err,res) => {
+  console.log(res);
+})
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+// var decode = jwt.verify(token,'123abc');
+// console.log('Decoded: ', decode);
 
 
 //
