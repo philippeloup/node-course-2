c467e84160d48e3fd3b63319827fe8f4fa917a9c
Philippe Loup <philippe.loup@gmail.com>
2018-04-26 23:32:47 -0400

    add test for POST login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -210,7 +210,7 @@ describe('POST /users', () =>{
         expect(user).toBeTruthy();
         expect(user.password).not.toEqual(password);
         done();
-      })
+      }).catch((e) => done(e));
     })
   })
 
@@ -234,3 +234,47 @@ describe('POST /users', () =>{
     .end(done)
   })
 })
+
+describe('POST /users/login', ()=>{
+  it('should login a user and return auth token',(done) =>{
+    var email = users[1].email;
+    var password = users[1].password;
+    request(app)
+    .post('/users/login')
+    .send({email, password})
+    .expect(200)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toBeTruthy();
+      expect(res.body._id).toBeTruthy();
+      expect(res.body.email).toBe(email);
+    })
+    .end((err,res) => {
+      if (err) {
+        return done(err);
+      }
+
+      User.findById(users[1]._id).then((user) => {
+        expect(user.tokens[0]).toEqual(expect.objectContaining({
+          access:'auth',
+          token: res.headers['x-auth']
+        }));
+        done();
+      }).catch((e) => done(e));
+    })
+  });
+
+  it('should reject invalid login',(done) => {
+    var email = users[1].email+'v';
+    var password = users[1].password;
+    request(app)
+    .post('/users/login')
+    .send({email, password})
+    .expect(400)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toBeFalsy();
+      expect(res.body.tokens).toBeFalsy();
+    })
+    .end(done);
+  })
+
+});
