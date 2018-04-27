52fb3d8d93ac44d47f33f2fc98c9d0a3ca0d06f3
Philippe Loup <philippe.loup@gmail.com>
2018-04-26 22:40:04 -0400

    POST login

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ require('./config/config');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -115,6 +117,17 @@ app.post('/users',(req,res) => {
 
 app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
+})
+
+app.post('/users/login',(req, res) =>{
+  var body = _.pick(req.body, ['email','password']);
+  User.findByCredentials(body.email, body.password).then((user)=> {
+    return user.generateAuthToken().then((token)=> {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) =>{
+    res.status(400).send()
+  })
 })
 
 app.listen(port, () => {
